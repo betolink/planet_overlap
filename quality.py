e50ffaf626c8aa0445e6be4964ac3c3be7aeefb4
Bri <104585874+BriannaLind@users.noreply.github.com>
2026-02-18 10:50:40 -0600

    Create quality.py

---
+++ quality.py
@@ -0,0 +1,11 @@
+from typing import List, Dict
+
+def filter_quality(properties: List[Dict],
+                   min_view_angle: float,
+                   max_cloud: float) -> List[Dict]:
+
+    return [
+        p for p in properties
+        if p["view_angle"] < min_view_angle
+        and p["cloud_cover"] <= max_cloud
+    ]
